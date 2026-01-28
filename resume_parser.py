eb22a3a437e3536494684e93a1081cda7e9f7652
GautamRaj1234 <147736953+GautamRaj1234@users.noreply.github.com>
2026-01-28 19:03:16 +0530

    Configure NLTK data directory and downloads
    Set up NLTK data directory and handle downloads.

--- resume_parser.py
+++ resume_parser.py
@@ -2,9 +2,21 @@ import pdfplumber
 import docx
 import re
 import nltk
+import os
 
-nltk.download('punkt')
-nltk.download('stopwords')
+nltk_data_dir = "/tmp/nltk_data"
+os.makedirs(nltk_data_dir, exist_ok=True)
+nltk.data.path.append(nltk_data_dir)
+
+try:
+    nltk.data.find("tokenizers/punkt")
+except LookupError:
+    nltk.download("punkt", download_dir=nltk_data_dir)
+
+try:
+    nltk.data.find("corpora/stopwords")
+except LookupError:
+    nltk.download("stopwords", download_dir=nltk_data_dir)
 
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
